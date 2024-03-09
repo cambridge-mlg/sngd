6cee68e83d1c7e2e47023258d2d1c78ad0cdae2c
Jonathan So <jonny.so@gmail.com>
2024-03-09 00:05:13 +0000

    reimplement gamma.natparams using jaxopt.implicit_diff.custom_root

--- sngd/distributions/ef/gamma.py
+++ sngd/distributions/ef/gamma.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 from jax import vmap
-from jaxopt import FixedPointIteration
+from jaxopt import implicit_diff
 from sngd.util.controlflow import bounded_while_loop
 from sngd.util.tree import tree_map, tree_scale, tree_sub
 
@@ -48,29 +48,32 @@ def sample(key, natparams, shape=()):
     return jax.random.gamma(key, a, (*shape, *a.shape))/b
 
 def natparams(meanparams):
+    # handle batching over leading dimensions
     shape_prefix = meanparams[0].shape
     meanparams = tree_map(lambda _: _.reshape(-1, *_.shape[len(shape_prefix):]), meanparams)
 
     def _natparams(meanparams):
         # f is concave, strictly increasing, tends to -inf as b -> 0 from above, and has
         # exactly one root when meanparams lie in the domain of valid mean parameters.
-        f = lambda m, a: jax.scipy.special.digamma(a) - jnp.log(a) + jnp.log(m[1]) - m[0]
+        f = lambda a, mp: jax.scipy.special.digamma(a) - jnp.log(a) + jnp.log(mp[1]) - mp[0]
         fprime = lambda a: jax.scipy.special.polygamma(1, a) - 1/a
-        # some parameters inside the mean domain can fail to converge for numerical reasons
+        # find a starting point alpha0 > 0 such that f is below 0. note that some parameters
+        # inside the mean domain can fail to converge for numerical reasons, hence the bound.
         alpha0 = bounded_while_loop(
-            cond=lambda _: f(meanparams, _) >= 0,
+            cond=lambda _: f(_, meanparams) >= 0,
             body=lambda _: .5*(_),
             init=2.0,
-            maxiter=50)
-        # use fixed point solver for efficient reverse-mode derivatives, but use our own
-        # loop to find the starting point so that we can use a custom termination condition
-        alpha = bounded_while_loop(
-            cond=lambda _: jnp.abs(f(meanparams, _)) > 1e-12,
-            body=lambda _: _ - f(meanparams, _)/fprime(_),
-            init=alpha0,
-            maxiter=50)
-        solver = FixedPointIteration(lambda _, mp: _ - f(mp, _)/fprime(_), maxiter=1)
-        alpha = solver.run(jax.lax.stop_gradient(alpha), mp=meanparams).params
+            maxiter=100)
+        # define custom_root condition to get efficient reverse-mode derivatives from jaxopt.
+        @implicit_diff.custom_root(f)
+        def _newton_solve(alpha0, mp):
+            return bounded_while_loop(
+                cond=lambda _: jnp.abs(f(_, mp)) > 1e-12,
+                body=lambda _: _ - f(_, mp)/fprime(_),
+                init=alpha0,
+                maxiter=100)
+        # newton's method is guaranteed to converge to a root of f from our starting point.
+        alpha = _newton_solve(jax.lax.stop_gradient(alpha0), meanparams)
         beta = alpha/meanparams[1]
         return natparams_from_standard((alpha, beta))
     
